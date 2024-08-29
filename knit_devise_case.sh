be3a033a235898777b5133e5606e9d6fd8ab6a22
AndySibov <sibov.andy@gmail.com>
2024-08-29 17:05:03 +0200

    fix

--- knit_devise_case.sh
+++ knit_devise_case.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-Rscript -e " x <- rmarkdown::render('/Users/andysibov/Desktop/VS code/devise_case.Rmd',
-output_format = 'pdf_document', output_dir = '/Users/andysibov/Desktop', output_file = "rapport_devise.pdf")
+Rscript -e "
+rmd_file <- '/Users/andysibov/Desktop/VS code/devise_case.Rmd'
+rmarkdown::render(rmd_file)
 "
+
